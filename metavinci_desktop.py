19dfbbd41fbe5a55687c2e9717854fe547fbee50
Jason <dimensions3@outlook.com>
2024-10-30 15:54:27 -0700

    add npm and dfx checks

--- metavinci_desktop.py
+++ metavinci_desktop.py
@@ -7,7 +7,10 @@ from easygui import *
 BRAND = "HEAVYMETA®"
 VERSION = "0.01"
 ABOUT = f"""
-Command Line Interface for {BRAND} Standard NFT Data
+Metavinci daemon by {BRAND}
+A toolset for Data Standards 
+and Metadata visualization
+validation and interaction.
 Version: {VERSION}
 ALL RIGHTS RESERVED 2024
 """
@@ -18,11 +21,32 @@ HOME = os.path.expanduser('~')
 METAVINCI_PATH = os.path.join(HOME, '.metavinci')
 METAVINCI_INSTALL = "curl -L https://raw.githubusercontent.com/inviti8/metavinci/main/install.sh | bash"
 METAVINCI_BIN = os.path.join(METAVINCI_PATH, 'bin', 'metavinci')
+DFX_BIN = os.path.join(HOME, '.local', 'share', 'dfx', 'bin', 'dfx')
 LOADING_IMG = os.path.join(FILE_PATH, 'images', 'loading.gif')
 
-def Mbox(text):
+def MsgBox(text):
+  choices = ["OK"]
+  return buttonbox(text, choices=choices)
+
+def InstallBox(text):
   choices = ["INSTALL","CANCEL"]
   return buttonbox(text, choices=choices)
+
+def _node_installed():
+    try:
+        process = run(['node', '--version'], stdout=PIPE, stderr=STDOUT)
+        if process.returncode == 0:
+            print("Node.js is installed.")
+            return True
+        else:
+            print("Node.js is not installed.")
+            return False
+    except FileNotFoundError:
+        print("Node.js is not installed.")
+        return False
+
+def _dfx_installed():
+  return os.path.isfile(DFX_BIN)
 
 def _run_command(cmd):
       process = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
@@ -34,15 +58,26 @@ def _run_command(cmd):
         print(output.decode('utf-8'))
 
 if __name__ == "__main__":
-    if not os.path.exists(METAVINCI_PATH):
-        print("metavinci not installed, installing...")
-        install = Mbox(f'{ABOUT}')
-        if install == 'INSTALL':
-          loading = GifAnimation(LOADING_IMG)
-          loading.Play()
-          _run_command(METAVINCI_INSTALL)
-          loading.Stop()
-          _run_command(METAVINCI_BIN)
-    else:
-        _run_command(METAVINCI_BIN)
+  _run = True
+
+  if not _node_installed():
+    MsgBox("Node is not installed, please install")
+    _run = False
+
+  if not _dfx_installed():
+    MsgBox("Dfx is not installed, please install")
+    _run = False
+
+  if _run and not os.path.exists(METAVINCI_PATH):
+    print("metavinci not installed, installing...")
+    install = InstallBox(f'{ABOUT}')
+    if install == 'INSTALL':
+      loading = GifAnimation(LOADING_IMG)
+      loading.Play()
+      _run_command(METAVINCI_INSTALL)
+      loading.Stop()
+      _run_command(METAVINCI_BIN)
+  else:
+    if _run:
+      _run_command(METAVINCI_BIN)
 
